f8845556436ea7999a970c852823fce9826804a7
tianye <2097248164@qq.com>
2024-05-12 00:18:25 +0800

    first

--- src/main/java/com/dao/NewsDao.java
+++ src/main/java/com/dao/NewsDao.java
@@ -1,22 +1,22 @@
 package com.dao;
 
-
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.baomidou.mybatisplus.plugins.pagination.Pagination;
-import com.entity.YonghuEntity;
-import com.entity.view.YonghuView;
+import com.entity.NewsEntity;
+import com.entity.view.NewsView;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
 
+
 /**
- * 用户 Dao 接口
+ * 公告信息 Dao 接口
  *
  * @author 
  */
-public interface YonghuDao extends BaseMapper<YonghuEntity> {
+public interface NewsDao extends BaseMapper<NewsEntity> {
 
-   List<YonghuView> selectListView(Pagination page,@Param("params")Map<String,Object> params);
+   List<NewsView> selectListView(Pagination page,@Param("params")Map<String,Object> params);
 
 }
