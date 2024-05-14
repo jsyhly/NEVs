03645f7c6cef82b900972363fc14dff5130dd3e1
tianye <2097248164@qq.com>
2024-05-14 20:34:22 +0800

    --story=1021667 --user=田野 点击缩略图跳转至详情页 https://www.tapd.cn/53137435/s/1119588

--- src/main/java/com/service/impl/UsersServiceImpl.java
+++ src/main/java/com/service/impl/UsersServiceImpl.java
@@ -2,21 +2,19 @@
 package com.service.impl;
 
 
-import java.util.List;
-import java.util.Map;
-
-import com.service.UsersService;
-import org.springframework.stereotype.Service;
-
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.dao.UsersDao;
 import com.entity.UsersEntity;
+import com.service.UsersService;
 import com.utils.PageUtils;
 import com.utils.Query;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
 
 
 /**
